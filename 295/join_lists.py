bafafdb25685fe9dd3648d1ac28e7c220b73ea33
rodrigo bernardo medeiros <rodrimedeiros@gmail.com>
2024-02-22 17:01:05 -0300

    Solve bite 295

--- 295/join_lists.py
+++ 295/join_lists.py
@@ -2,4 +2,11 @@ from typing import List, Union
 
 
 def join_lists(lst_of_lst: List[List[str]], sep: str) -> Union[List[str], None]:
-    ...
+    if not lst_of_lst:
+        return None
+    complete_lst = []
+    for lst in lst_of_lst:
+        complete_lst.extend(lst)
+        complete_lst.append(sep)
+    del complete_lst[-1]
+    return complete_lst
