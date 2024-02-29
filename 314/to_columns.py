a460b647600528812cf79928500f4258f5dbe0b3
rodrigo bernardo medeiros <rodrimedeiros@gmail.com>
2024-02-28 22:04:01 -0300

    Solve the bite 314

--- 314/to_columns.py
+++ 314/to_columns.py
@@ -2,4 +2,13 @@ from typing import List  # not needed when we upgrade to 3.9
 
 
 def print_names_to_columns(names: List[str], cols: int = 2) -> None:
-    pass
+
+    counter = 0
+    for name in names:
+        name_with_pipe = "| " + name.ljust(10)
+        if counter < cols - 1:
+            print(name_with_pipe, end="")
+            counter += 1
+        else:
+            print(name_with_pipe)
+            counter = 0
